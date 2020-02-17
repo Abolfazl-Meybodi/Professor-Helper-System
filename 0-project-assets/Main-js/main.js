1ec7382f34d28a7a5f4e114854f41f74fae92316
abolfazl <abolfazl.meibody@gmail.com>
2020-02-18 00:28:46 +0330

    update1

--- 0-project-assets/Main-js/main.js
+++ 0-project-assets/Main-js/main.js
@@ -469,11 +469,11 @@ if (window.XMLHttpRequest) {
 } else {
     xhttp = new ActiveXObject();
 }
-xhttp.onreadystatechange = function () {
-    if ((this.readyState === 4) && (this.status === 200)) {
-        document.getElementById("UpdateCalender").innerHTML =
-        this.responseText;
-        }   
-    }
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    document.getElementById("UpdateCalender").innerHTML =
+    xhttp.responseText;
+  }
+};
 xhttp.open("GET", "data.txt", true);
 xhttp.send();
